72808f0dc495ca305b1e24c6bbd98de66efcee41
Mac <570768995@qq.com>
2018-08-23 16:59:38 +0800

    fix

--- assets/game/script/managers/gameManager.js
+++ assets/game/script/managers/gameManager.js
@@ -229,6 +229,7 @@ cc.Class({
     },
 
     logoutResponse: function(status) {
+        Game.GameManager.network.disconnect();
         cc.game.removePersistRootNode(this.node);
         cc.director.loadScene('lobby');
     },
