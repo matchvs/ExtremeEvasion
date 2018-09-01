4a1c3e2c363617b70930e3cde2d979e26dabcfca
Mac <570768995@qq.com>
2018-09-01 10:25:48 +0800

    fix

--- build/wechatgame/game.js
+++ build/wechatgame/game.js
@@ -4,5 +4,5 @@ window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
 wxDownloader.REMOTE_SERVER_ROOT = "https://data.tianziyou.com/matchvsGamesRes/extremeEvasion";
 wxDownloader.SUBCONTEXT_ROOT = "";
-require('src/settings.9d13c');
-require('main.21cb0');
+require('src/settings.ec4c2');
+require('main.3b338');
